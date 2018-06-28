38e98ca60aab569ac00008ff4348fbb0a601aeda
Vito Flavio Lorusso <vf.lorusso@live.com>
2018-06-28 14:00:21 +0200

    added bastion configuration

---
+++ bastion-linux-vm/configureBastionVM.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+yum install epel-release -y
+yum install clamav-server clamav-data clamav-update clamav-filesystem clamav clamav-scanner-systemd clamav-devel clamav-lib clamav-server-systemd -y
